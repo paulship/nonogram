a1d316667111b7b554ded29e8fc3955dc089e714
Paul Shipley <paul.shipley@blueyonder.co.uk>
2021-01-10 22:58:50 +0000

    Fixing various compile issues.

--- nonogram_init.c
+++ nonogram_init.c
@@ -2,23 +2,26 @@
 #include <stdint.h>
 
 #include "nonogram.h"
+#include "nonogram_init.h"
 
-static void nonogram_init( GAME_T* my_game )
+static void compute_regions( CELLSTRIP_T* cellstrip );
+
+void nonogram_init( GAME_T* my_game )
 {
     const uint16_t n_strips = my_game->rows + my_game->columns;
 
     for( uint16_t n=0; n<n_strips; n++ )
     {
-        compute_regions( my_game->strips[n] );
+        compute_regions( my_game->strips + n );
     }
 }
 
-void compute_regions( CELLSTRIP_T* cellstrip )
+static void compute_regions( CELLSTRIP_T* cellstrip )
 {
     cellstrip->possible_list = NULL;
     struct POSSIBLE_T** next_ptr = &cellstrip->possible_list;
     
-    
+    (void)next_ptr;
 
 }
 
