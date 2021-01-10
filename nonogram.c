836982937d51efd17e11f9af1c64f99dd1b343e2
Paul Shipley <paul.shipley@blueyonder.co.uk>
2021-01-10 22:45:14 +0000

    Remove unrequired function.

--- nonogram.c
+++ nonogram.c
@@ -7,23 +7,14 @@
 #include "nonogram_create.h"
 #include "nonogram_output.h"
 
+
 int main( void )
 {
     GAME_T* my_game = create_game();
-
-    compute_regions(my_game);
 
     output_game( my_game );
 
     return 0;
 }
 
-static void compute_regions( GAME_T* my_game )
-{
-    const uint16_t n_strips = my_game->rows + my_game->columns;
-
-    for( uint16_t n=0; n<n_strips; n++ )
-    {
-    }
-}
 
