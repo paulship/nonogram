505332030398baceaccdbe2a1399c02fb2372c86
Paul Shipley <paul.shipley@blueyonder.co.uk>
2021-01-24 21:59:09 +0000

    Update spacing.

--- nonogram.c
+++ nonogram.c
@@ -7,10 +7,11 @@
 #include "nonogram_create.h"
 #include "nonogram_output.h"
 
-
 int main( void )
 {
     GAME_T* my_game = create_game();
+
+    compute_regions(my_game);
 
     output_game( my_game );
 
